e71abec59fede31b778c44decdae24a771f200b8
Cyri0 <37551641+Cyri0@users.noreply.github.com>
2022-03-17 18:43:25 +0100

    2022.03.17

---
+++ ADFGVXgui/MainWindow.xaml.cs
@@ -0,0 +1,139 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ADFGVXgui
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+
+    public class Betu
+    {
+        public char betu;
+        public int darab;
+
+        public Betu(char betu)
+        {
+            this.betu = betu;
+            this.darab = 0;
+        }
+    }
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void betoltes_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+
+            if(ofd.ShowDialog() == true)
+            {
+                string file = ofd.FileName;
+
+                // FÁJLBEOLVASÁS
+                List<string> sorok = new List<string>();
+                StreamReader sr = new StreamReader(file);
+                while (!sr.EndOfStream)
+                {
+                    sorok.Add(sr.ReadLine());
+                }
+
+                // KIÍRATÁS
+                betoltottKodtabla.Content = "";
+                foreach (string sor in sorok)
+                {
+                    foreach (char betu in sor)
+                    {
+                        betoltottKodtabla.Content += betu + " ";
+                    }
+                    betoltottKodtabla.Content += "\n";
+                }
+
+                // HIBAKERESÉSEK
+                listaDoboz.Items.Clear();
+
+                if(sorok.Count == 6) {
+                    foreach (string sor in sorok)
+                    {
+                        if(sor.Length != 6)
+                        {
+                            listaDoboz.Items.Add("Hiba a mátrix méretében!");
+                            break;
+                        }
+                    }
+                }
+                else
+                {
+                    listaDoboz.Items.Add("Hiba a mátrix méretében!");
+                }
+
+                // CSAK BETŰK ÉS SZÁMOK
+
+                foreach (string sor in sorok)
+                {
+                    foreach (char betu in sor)
+                    {
+                        if (!(betu >= 'a' && betu <= 'z' ||
+                            betu >= '0' && betu <= '9'))
+                        {
+                            listaDoboz.Items.Add($"Hibás karakter ({betu}) van a mátrixban!");
+                        }
+                    }
+                }
+
+                // Minden betű egyszer
+                List<Betu> betulista = new List<Betu>();
+                for (char i = 'a'; i <= 'z'; i++)
+                {
+                    betulista.Add(new Betu(i));
+                }
+                for(char i = '0'; i <= '9'; i++)
+                {
+                    betulista.Add(new Betu(i));
+                }
+
+                foreach (string sor in sorok)
+                {
+                    foreach (char betu in sor)
+                    {
+                        foreach (Betu item in betulista)
+                        {
+                            if(item.betu == betu)
+                            {
+                                item.darab++;
+                                break;
+                            }
+                        }
+                    }
+                }
+
+                foreach (Betu item in betulista)
+                {
+                    if(item.darab != 1)
+                    {
+                        listaDoboz.Items.Add($"A(z) {item.betu} karakter {item.darab}x szerepel a mátrixban!");
+                    }
+                }
+
+
+            }
+        }
+    }
+}
---
+++ karacsonyGUI/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace karacsonyGUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private int raktarkeszlet = 0;
+
+        private void toltNapokSzama(int kezdo) {
+            napokSzama.Items.Clear();
+            for (int i = kezdo; i <= 40; i++)
+            {
+                napokSzama.Items.Add(i);
+            }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            toltNapokSzama(1);
+        }
+
+        private void hozzaad_Click(object sender, RoutedEventArgs e)
+        {
+            errorLabel.Content = "";
+            try
+            {
+                int elkeszult = int.Parse(elkeszitett.Text);
+                int eladva = int.Parse(eladott.Text);
+
+                if(elkeszult >= 0 && eladva >= 0) {
+                    if(raktarkeszlet + elkeszult - eladva >= 0)
+                    {
+                        raktarkeszlet = raktarkeszlet + elkeszult - eladva;
+
+                        listaDoboz.Items.Add($"{napokSzama.Text}.nap:\t+{elkeszitett.Text}\t-{eladott.Text}\t=\t{raktarkeszlet}");
+
+                    toltNapokSzama(int.Parse(napokSzama.Text) + 1);
+                    elkeszitett.Text = "0";
+                    eladott.Text = "0";
+                    }
+                    else
+                    {
+                    errorLabel.Content = "Túl sok eladott angyalka!";
+                    }
+                }
+                else
+                {
+                    errorLabel.Content = "Negatív számot nem adhat meg!";
+                }
+
+            }
+            catch (Exception)
+            {
+                errorLabel.Content = "Adj meg érvényes számokat!";
+            }
+
+
+
+
+        }
+    }
+}
