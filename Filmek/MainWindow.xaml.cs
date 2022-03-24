544a31c35e22a74bfa3eef128c09e2fe79b9ab82
Cyri0 <37551641+Cyri0@users.noreply.github.com>
2022-03-24 17:19:44 +0100

    20220324-halftime

---
+++ Filmek/MainWindow.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Filmek
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<Movie> movies;
+
+        private void initTypes()
+        {
+            typeUI.Items.Add("title");
+            typeUI.Items.Add("director");
+            typeUI.Items.Add("actor/actress");
+            typeUI.SelectedIndex = 0;
+        }
+
+        private void read() {
+            try
+            {
+                StreamReader sr = new StreamReader("movies.txt");
+                sr.ReadLine(); // eldobjuk az első sort
+
+                while (!sr.EndOfStream)
+                {
+                    movies.Add(new Movie(sr.ReadLine()));
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Missing or invalid file!");
+            }
+        }
+
+        public MainWindow()
+        {
+            movies = new List<Movie>();
+            InitializeComponent();
+            initTypes();
+            read();
+        }
+
+        private void searchBarUI_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            matchMoviesUI.Items.Clear();
+
+            if(searchBarUI.Text != "")
+            {
+                foreach (Movie item in movies)
+                {
+                    if (item.title.ToUpper().Contains(searchBarUI.Text.ToUpper()))
+                    {
+                        matchMoviesUI.Items.Add(item);
+                    }
+                }
+            }
+        }
+
+        private void matchMoviesUI_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Movie choosen = (Movie)matchMoviesUI.SelectedItem;
+
+            if(choosen != null)
+            {
+                movieTitleUI.Content = choosen.title;
+                directorUI.Content = choosen.director;
+                castUI.Content = choosen.cast;
+                rateUI.Content = "10/" + choosen.vote_average;
+            }
+        }
+    }
+}
---
+++ Filmek/Movie.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Filmek
+{
+    class Movie
+    {
+        public string revenue, vote_average, vote_count, title, original_language, release_date, production_companies, production_countries, genres, director, producer, cast, runtimes, writer;
+
+        public Movie(string line) {
+            string[] parts = line.Split(';');
+
+            (revenue, vote_average, vote_count, title, original_language, release_date, production_companies, production_countries, genres, director, producer, cast, runtimes, writer) = (parts[0], parts[1], parts[2], parts[3], parts[4], parts[5], parts[6], parts[7], parts[8], parts[9], parts[10], parts[11], parts[12], parts[13]);
+        }
+
+        public override string ToString()
+        {
+            return this.title;
+        }
+    }
+}
